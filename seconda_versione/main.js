7a2326d4d11f53552d9e0b3b110592bbeeae9cc8
Bilobra <bra.carrozzo@gmail.com>
2022-09-23 16:41:25 +0200

    add axios in js

--- seconda_versione/main.js
+++ seconda_versione/main.js
@@ -1,14 +1,23 @@
 const app = new Vue({
     el:'#app',
     data:{
-
+        dischi:[],
     },
 
     methods:{
-
+        fetchDisk(){
+            axios 
+            .get('http://localhost/boolean/php-ajax-dischi/php-ajax-dischi/seconda_versione/api/dischi.php')
+            .then((res)=>{
+                const {response} = res.data
+                this.dischi = response
+            })
+        },  
     },
 
     created(){
-        
+
+        this.fetchDisk()
+
     }
 })
